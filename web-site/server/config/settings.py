306065fc1205ce29d3d5f109d79591f259f9b20e
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2024-01-04 04:08:19 +0500

    main

--- web-site/server/config/settings.py
+++ web-site/server/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['api.svetafor.uz']
+ALLOWED_HOSTS = ['api.svetafor.uz', '18.212.239.39']
 
 
 # Application definition

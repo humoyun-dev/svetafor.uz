0ab0ea7018123cec3efb02d50174138191daed62
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2024-01-08 22:04:43 +0500

    main

---
+++ telegram-bot/client/src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button.tsx";
+
+const App = () => {
+    return (
+        <div>
+            <Button>Hello</Button>
+        </div>
+    );
+};
+
+export default App;

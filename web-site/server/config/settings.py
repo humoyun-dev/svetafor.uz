642c55da4ed48cacb8c5e1dff0801e19453c2b1c
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2024-02-19 13:43:17 +0500

    main

--- web-site/server/config/settings.py
+++ web-site/server/config/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
-    # "https://www.svetafor.uz/"
+    "https://www.svetafor.uz/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True

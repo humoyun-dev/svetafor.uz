8de98b70628c0c48ad4427b39343dd396ebd469b
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2024-02-19 15:26:42 +0500

    main

--- web-site/server/config/settings.py
+++ web-site/server/config/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'django_elasticsearch_dsl',
+    # 'django_elasticsearch_dsl',
 ]
 
 MIDDLEWARE = [
